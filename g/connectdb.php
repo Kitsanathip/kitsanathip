53d4f3239e5ec87d3e6f2457d4be4722b585d0f6
Kitsanathip <mart3220@gmail.com>
2026-02-13 23:25:13 +0700

    Update connectdb.php

--- g/connectdb.php
+++ g/connectdb.php
@@ -1,7 +1,7 @@
 <?php
 	$host = "localhost";
-	$user = "root";
-	$pwd = "";
+	$user = "Admin";
+	$pwd = "Password";
 	$db = "4089db";
 	$conn = mysqli_connect($host,$user,$pwd,$db) or die("เชื่อมต่อฐานข้อมูลไม่ได้");
 	mysqli_query($conn,"SET NAMES utf8");
